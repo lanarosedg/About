c1a7e1eff562641f8c4cd70220bf8c773bc95750
lanarosedeguzman@gmail.com <lanarosedeguzman@gmail.com>
2025-06-04 15:48:53 +0800

    Mobile responsiveness update

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -3,7 +3,7 @@ function Introduction() {
         <div className="introContainer">
             <p className="introText">
                 A full-stack web developer specializing in creating
-                <strong>interactive</strong> and <strong>intuitive</strong> websites that deliver a sleek
+                <strong> interactive</strong> and <strong>intuitive</strong> websites that deliver a sleek
                 User Interface (UI) and User Experience (UX). With
                 expertise in <strong>React.js, Java, and C#</strong>, I build dynamic
                 and user-centric web solutions.</p>
